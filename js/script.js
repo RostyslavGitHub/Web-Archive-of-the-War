3293df7f04e62a35bcbdb23604aa98f92f4fca14
RostyslavGitHub <117546122+RostyslavGitHub@users.noreply.github.com>
2023-03-11 21:01:01 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,24 +5,36 @@ const burgerMenu = document.querySelector('.burger_menu');
 if (burgerMenu){
 	burgerMenu.addEventListener("click", function (e) {
 		menu.classList.toggle('_active-menu');
-	});
-
-	burgerMenu.addEventListener("click", function (e) {
 		burgerMenu.classList.toggle('_close-burger_menu_icon');
 	});
-}
+};
 
-
+let closeMenu = () => {
+		menu.classList.remove('_active-menu');
+		burgerMenu.classList.remove('_close-burger_menu_icon');
+		closeArea.classList.remove('_close_area');
+};
 
 const closeArea = document.querySelector('.to-close');
-let closeAreaHeight = () => {
+let siteHeightToCloseAreaHeight = () => {
 	let scrollHeight = Math.max(
   document.body.scrollHeight, document.documentElement.scrollHeight,
   document.body.offsetHeight, document.documentElement.offsetHeight,
   document.body.clientHeight, document.documentElement.clientHeight);
 
   closeArea.style.height=scrollHeight+'px';
-}
+};
+
+if(closeArea){
+	siteHeightToCloseAreaHeight();
+	burgerMenu.addEventListener("click", function (e) {
+		closeArea.classList.toggle('_close_area');
+	});
+
+	closeArea.addEventListener("click", function (e) {
+		closeMenu();
+	});
+};
 
 const whyButton = document.getElementById('whyButton');
 const blockWhy = document.getElementById('why');
@@ -36,7 +48,7 @@ const blockHow = document.getElementById('how');
 		blockWhat.style.display="none";
 		blockHow.style.display="none";
 		blockWhy.scrollIntoView();
-		closeAreaHeight();
+		siteHeightToCloseAreaHeight();
 	});
 
 	whatButton.addEventListener("click", function (e) {
@@ -44,7 +56,7 @@ const blockHow = document.getElementById('how');
 		blockWhy.style.display="none";
 		blockHow.style.display="none";
 		blockWhat.scrollIntoView();
-		closeAreaHeight();
+		siteHeightToCloseAreaHeight();
 	});
 
 	howButton.addEventListener("click", function (e) {
@@ -52,24 +64,8 @@ const blockHow = document.getElementById('how');
 		blockWhat.style.display="none";
 		blockWhy.style.display="none";
 		blockHow.scrollIntoView();
-		closeAreaHeight();
+		siteHeightToCloseAreaHeight();
 	});
-
-if(closeArea){
-	closeAreaHeight();
-	burgerMenu.addEventListener("click", function (e) {
-		closeArea.classList.toggle('_close_area');
-	});
-
-	closeArea.addEventListener("click", function (e) {
-		menu.classList.remove('_active-menu');
-		burgerMenu.classList.remove('_close-burger_menu_icon');
-		closeArea.classList.remove('_close_area');
-	});
-};
-
-
-
 
 const showMoreInfo = document.getElementsByClassName('catalog__item__name');
 if(showMoreInfo){
@@ -78,51 +74,43 @@ if(showMoreInfo){
 		const moreInfo = document.getElementsByClassName('item__more-info');
 		moreInfo[i].classList.toggle('_show-more-info');
 	});
-}}
+}};
 
 let itemDescription = document.querySelectorAll('.catalog__item__description');
 let itemDescriptionScroll = [];
 if(itemDescription){
 	for (let i = 0; i < itemDescription.length; i++){
 		itemDescriptionScroll.push(itemDescription[i]);
-		let num = itemDescriptionScroll.indexOf();
 		if(itemDescription[i].scrollHeight > itemDescription[i].clientHeight){
 		   itemDescription[i].style.padding="10px";
 		   itemDescription[i].style.justifyContent="start";
 		}
-}}
+}};
 
 let menuItem = document.getElementsByClassName('menu_item');
 if(menuItem){
 	for(let i = 0; i < menuItem.length; i++){
 		menuItem[i].addEventListener("click", function () {
-			menu.classList.remove('_active-menu');
-			burgerMenu.classList.remove('_close-burger_menu_icon');
-			closeArea.classList.remove('_close_area');
+			closeMenu();
 		});
 	}};
 
-
-
-
-/*const scrUp = document.querySelector('.scroll-up');
-if (scrUp){
-	let scrollpos = window.scrollY;
-	const scrollButton = document.querySelector(".scroll-up");
-	let scrollChange = 340;
-	const add_class_on_scroll = () => scrollButton.classList.add("_show-scroll-up");
-	window.addEventListener('scroll', function() { 
-	  scrollpos = window.scrollY;
-
-	  if (scrollpos >= scrollChange) { add_class_on_scroll(); }
+function scrollToTop() {
+	window.scrollTo({
+		top: 0,
+		behavior: 'smooth'
 	});
-
-	const remove_class_on_scroll = () => scrollButton.classList.remove("_show-scroll-up");
-	window.addEventListener('scroll', function() { 
-	  scrollpos = window.scrollY;
-
-	  if (scrollpos >= scrollChange) { add_class_on_scroll(); }
-	  else { remove_class_on_scroll(); }
-	  
-	});
-}*/
+	if(window.innerWidth < 993){
+		closeMenu();
+	}
+};
+window.onscroll = function() {
+	showScrollButton();
+};
+function showScrollButton() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		document.querySelector('.scroll-to-top').style.display = 'block';
+	} else {
+		document.querySelector('.scroll-to-top').style.display = 'none';
+	}
+};
